1e6c86f165e78ded24298593b978a300d0337d00
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-14 01:24:25 -0300

    Create Imagen List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario'
+import ListadoImagenes from './components/ListadoImagenes'
 
 function App() {
 
   // state de la app
 
   const [ busqueda, guardarBusqueda ] = useState('');
+  const [ imagenes, guardarImagenes ] = useState([]);
 
   // useEffect
   useEffect(() => {
    const consultarApi = async () => {
     if(busqueda === '') return;
 
-    const imagenesPorPagina = 30;
+    const imagenesPorPagina = 10;
     const key = '22045805-39d9ab781e3daa10a0055cd54';
     const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
 
     const respuesta = await fetch(url);
     const resultado = await respuesta.json();
 
-    guardarBusqueda(resultado.hits);
+    guardarImagenes(resultado.hits);
    }
    consultarApi();
 
@@ -32,6 +34,14 @@ function App() {
 
         <Formulario 
           guardarBusqueda={guardarBusqueda}
+        />
+
+        
+      </div>
+
+      <div className="row justify-content-center">
+        <ListadoImagenes 
+          imagenes={imagenes}
         />
       </div>
     </div>    
