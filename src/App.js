d863abe911811e4be7f243084c1ce2ea1ec384d6
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-14 00:57:54 -0300

    Consult API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario'
 
 function App() {
+
+  // state de la app
+
+  const [ busqueda, guardarBusqueda ] = useState('');
+
+  // useEffect
+  useEffect(() => {
+   const consultarApi = async () => {
+    if(busqueda === '') return;
+
+    const imagenesPorPagina = 30;
+    const key = '22045805-39d9ab781e3daa10a0055cd54';
+    const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+
+    guardarBusqueda(resultado.hits);
+   }
+   consultarApi();
+
+  }, [busqueda])
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imágenes</p>
 
-        <Formulario />
+        <Formulario 
+          guardarBusqueda={guardarBusqueda}
+        />
       </div>
     </div>    
   );
