34d7a7e6b01f1fcd00eb49dd9027b130bb465d2c
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-14 01:45:22 -0300

    Show Images and URL for high resolutioN

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
    const consultarApi = async () => {
     if(busqueda === '') return;
 
-    const imagenesPorPagina = 10;
+    const imagenesPorPagina = 30;
     const key = '22045805-39d9ab781e3daa10a0055cd54';
     const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
 
